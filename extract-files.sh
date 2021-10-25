0a25214ab3b0d798a1cb29d87bc423c8efef3d59
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2021-10-25 13:43:26 +0530

    surya: Switch to AOSP WFD
     * Reference:
       https://github.com/LineageOS/android_device_asus_sake/commit/ce9e6dd001ed03edb677655dafc91221f2e27c4a
       https://github.com/LineageOS/android_device_asus_X01AD/commit/bd04298578534850464b9cb1736fbc74f3b21bfc

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,9 @@ function blob_fixup() {
         system_ext/lib64/libwfdnative.so)
             "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
+        vendor/bin/mi_thermald)
+            sed -i "s|ug_cpu|ug_uwu|g" "${2}"
+            ;;
         vendor/lib64/hw/camera.qcom.so)
             "${PATCHELF}" --remove-needed "libMegviiFacepp-0.5.2.so" "${2}"
             "${PATCHELF}" --remove-needed "libmegface.so" "${2}"
