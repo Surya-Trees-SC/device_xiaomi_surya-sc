93f44af026c559b6e8c9a54df4bf93fd21ab2e3e
TheStrechh <thestrechh@gmail.com>
2021-12-21 23:08:01 +0000

    surya: Add more stuff for DiracSound

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
 
+import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
@@ -34,6 +35,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
+	DiracUtils.initialize(context);
         DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
     }
