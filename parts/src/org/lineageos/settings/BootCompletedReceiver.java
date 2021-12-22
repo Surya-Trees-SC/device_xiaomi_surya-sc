32a294552a8f9f0a348cb2ad0a735a52ae023ca1
ᴄᴀʀʟᴏꜱ ᴀʀʀɪᴀɢᴀ <37526095+TheStrechh@users.noreply.github.com>
2021-12-22 00:12:12 -0600

    surya: parts: Don't crash the whole process if dirac doesn't start

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,21 +20,27 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
-import org.lineageos.settings.utils.FileUtils;
+import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
+
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (DEBUG)
-            Log.d(TAG, "Received boot completed intent");
+        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        try {
         DiracUtils.initialize(context);
+        } catch (Exception e) {
+            Log.d(TAG, "Dirac is not present in system");
+        }
+        DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
     }
 }
