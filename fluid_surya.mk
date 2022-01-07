1b569e6068cfd344eb7f68afabd10df2fba075b9
FabricioSC <89610142+FabricioSC123@users.noreply.github.com>
2022-01-07 16:05:31 -0500

    Update and rename aosp_surya.mk to fluid_surya.mk

--- fluid_surya.mk
+++ fluid_surya.mk
@@ -22,11 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common AospExtended stuff
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/fluid/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 WITH_GAPPS := true
 
-PRODUCT_NAME := aosp_surya
+PRODUCT_NAME := fluid_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3 NFC
