7f7398f5d09759e29c6487cedc645269f829a373
FabricioSC <89610142+FabricioSC123@users.noreply.github.com>
2022-01-14 16:04:02 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,4 @@
-# Proton
-git clone https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-13 --depth=1
-touch prebuilts/clang/host/linux-x86/clang-13/AndroidVersion.txt
-echo 13 > prebuilts/clang/host/linux-x86/clang-13/AndroidVersion.txt
+
 # firmware
 git clone https://gitlab.com/fabriciosc123pro/rom-surya-firmware.git firmware/xiaomi/surya
 # kernel
@@ -11,7 +8,6 @@ git clone  https://github.com/xiaomeme-surya/android_vendor_xiaomi_surya.git ven
 # devicesettings
 rm -rf packages/resources/devicesettings
 git clone https://github.com/xiaomeme-surya/packages_resources_devicesettings packages/resources/devicesettings
-
 # hardware
 git clone https://github.com/PixelExperience/hardware_xiaomi hardware/xiaomi
 # qcom
@@ -19,6 +15,5 @@ rm -rf hardware/qcom-caf/sm8150/audio
 rm -rf hardware/qcom-caf/sm8150/media
 rm -rf hardware/qcom-caf/sm8150/display
 git clone https://github.com/xiaomeme-surya/android_hardware_qcom_display.git  hardware/qcom-caf/sm8150/display
-
 git clone https://github.com/xiaomeme-surya/android_hardware_qcom_audio.git hardware/qcom-caf/sm8150/audio
 git clone https://github.com/xiaomeme-surya/android_hardware_qcom_media hardware/qcom-caf/sm8150/media
