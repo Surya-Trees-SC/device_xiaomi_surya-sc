b90e129e82f31183dbb5e8acde3d63d05a805bff
FabricioSC123 <fabriciosalaspaypal@gmail.com>
2022-01-31 11:01:48 -0500

    Use Pronto Clang-14

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,6 @@
 git clone https://gitlab.com/fabriciosc123pro/rom-surya-firmware.git firmware/xiaomi/surya
 # kernel
 git clone https://github.com/ghostrider-reborn/android_kernel_xiaomi_surya -b perf  kernel/xiaomi/surya
-
 # vendor
 git clone  https://github.com/xiaomeme-surya/android_vendor_xiaomi_surya.git vendor/xiaomi/surya
 # devicesettings
@@ -11,7 +10,11 @@ rm -rf packages/resources/devicesettings
 git clone https://github.com/xiaomeme-surya/packages_resources_devicesettings packages/resources/devicesettings
 # hardware
 git clone https://github.com/PixelExperience/hardware_xiaomi hardware/xiaomi
-# qcom
+#Proton
+git clone https://github.com/XSans02/XSansTC-Clang/ -b master prebuilts/clang/host/linux-x86/clang-14
+touch prebuilts/clang/host/linux-x86/clang-14/AndroidVersion.txt
+echo 14  > prebuilts/clang/host/linux-x86/clang-14/AndroidVersion.txt
+#Qcom-Caf
 rm -rf hardware/qcom-caf/sm8150/audio
 rm -rf hardware/qcom-caf/sm8150/media
 rm -rf hardware/qcom-caf/sm8150/display
