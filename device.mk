cd78a8f5a9193eb76df24021395dc4d1813ed2b9
TheStrechh <thestrechh@gmail.com>
2022-02-03 15:24:21 +0000

    Revert "Revert "surya: Build atrace service""
    This reverts commit d1b9bf979f33f28431f883c5337ead7c3c9fd03f.

--- device.mk
+++ device.mk
@@ -46,6 +46,10 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
+# Atrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
