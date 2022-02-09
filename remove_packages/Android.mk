6c62db0d99425f44ece9036b181ae170d0af6335
FabricioSC123 <fabriciosalaspaypal@gmail.com>
2022-02-09 16:35:12 +0000

    surya: fix error remove_packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
-        GoogleTTS \
+        GoogleTTS 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
