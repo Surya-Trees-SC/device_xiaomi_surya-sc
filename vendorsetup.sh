ab113042ee82937b62d22f6bd94e73090ccc07e8
FabricioSC <89610142+FabricioSC123@users.noreply.github.com>
2022-02-18 00:06:07 -0500

    surya: No use hardware and lanwchair

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,10 +4,6 @@ git clone https://gitlab.com/fabriciosc123pro/rom-surya-firmware.git firmware/xi
 git clone https://github.com/ArrowOS-Devices/android_kernel_xiaomi_surya kernel/xiaomi/surya
 # vendor
 git clone https://github.com/Surya-Trees-SC/vendor_xiaomi_surya-sc vendor/xiaomi/surya
-# hardware
-git clone https://github.com/PixelExperience/hardware_xiaomi hardware/xiaomi
-# Lawnchair
-git clone https://github.com/Surya-Trees-SC/vendor_lawnchair.git vendor/lawnchair
 # Add Ccache Fix
 export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
