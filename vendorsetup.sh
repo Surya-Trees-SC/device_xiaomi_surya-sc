b9a10a29bdd7af71561690b262bf6a91c3618e05
FabricioSC123 <fabriciosalaspaypal@gmail.com>
2022-02-20 22:54:14 -0500

    surya: More configs surya vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,27 +1,34 @@
+DIR=$(readlink -f "$0")
+HOME=$(dirname "$DIR")
 # firmware
 if [[ -e firmware/xiaomi/surya ]];then
-echo ""
+cd $HOME/firmware/xiaomi/surya
+git pull
 else
-git clone https://gitlab.com/fabriciosc123pro/rom-surya-firmware.git firmware/xiaomi/surya
+git clone https://gitlab.com/fabriciosc123pro/rom-surya-firmware.git $HOME/firmware/xiaomi/surya
 fi
 # kernel
 if [[ -e kernel/xiaomi/surya ]];then
-echo ""
+cd $HOME/kernel/xiaomi/surya
+git pull
 else
-git clone https://github.com/PixelExperience-Devices/kernel_xiaomi_surya.git kernel/xiaomi/surya
+git clone https://github.com/PixelExperience-Devices/kernel_xiaomi_surya.git $HOME/kernel/xiaomi/surya
 fi
 # vendor
 if [[ -e vendor/xiaomi/surya ]];then
-echo ""
+cd $HOME/vendor/xiaomi/surya
+git pull
 else
-git clone https://github.com/Surya-Trees-SC/vendor_xiaomi_surya-sc vendor/xiaomi/surya
+git clone https://github.com/Surya-Trees-SC/vendor_xiaomi_surya-sc $HOME/vendor/xiaomi/surya
 fi
 # hardware
 if [[ -e hardware/xiaomi ]];then
-echo ""
+cd $HOME/hardware/xiaomi/surya
+git pull
 else
-git clone https://github.com/PixelExperience/hardware_xiaomi hardware/xiaomi
+git clone https://github.com/PixelExperience/hardware_xiaomi $HOME/hardware/xiaomi
 fi
+cd $HOME
 # Add Ccache Fix
 export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
