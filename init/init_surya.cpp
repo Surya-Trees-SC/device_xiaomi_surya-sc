342a852da912ac8fd6fcd4e85f4328f23965970e
FabricioSC123 <fabriciosalaspaypal@gmail.com>
2022-02-23 07:24:07 +0000

    surya: update fp credit:@TheStrechh

--- init/init_surya.cpp
+++ init/init_surya.cpp
@@ -88,14 +88,14 @@ void vendor_load_properties() {
         model = "M2007J20CG";
         mod_device = "surya_global";
 	name = "surya_global";
-	fingerprint = "google/raven/raven:12/S2B3.220205.007.A1/8133516:user/release-keys";
+	fingerprint = "google/raven/raven:12/SQ1D.220205.004/8151327:user/release-keys";
 	description = "surya_eea-user 11 RKQ1.200826.002 V12.5.5.0.RJGEUXM release-keys";
     } else if (hwname == "karna") {
 	device = "karna";
         model = "M2007J20CI";
         mod_device = "surya_in_global";
 	name = "karna_in";
-	fingerprint = "google/raven/raven:12/S2B3.220205.007.A1/8133516:user/release-keys";
+	fingerprint = "google/raven/raven:12/SQ1D.220205.004/8151327:user/release-keys";
 	description = "surya_in-user 11 RKQ1.200826.002 V12.5.5.0.RJGEUXM release-keys";
    }
 
