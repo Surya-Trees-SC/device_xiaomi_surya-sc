e694508e39e239324cd5985e441df996e7c6e608
FabriSC <u22216510@utp.edu.pe>
2022-08-14 15:33:04 -0500

    Revert "changues post update"
    This reverts commit 1ea171949cd554dd36bd6fe6ae2c898c0f98a22a.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,21 +5,21 @@ if [[ -e firmware/xiaomi/surya ]];then
 cd $HOME/firmware/xiaomi/surya
 git pull
 else
-git clone https://gitlab.com/Ardjlon/firmware_xiaomi_surya.git $HOME/firmware/xiaomi/surya
+git clone https://gitlab.com/fabriciosc123pro/rom-surya-firmware.git $HOME/firmware/xiaomi/surya
 fi
 # kernel
 if [[ -e kernel/xiaomi/surya ]];then
 cd $HOME/kernel/xiaomi/surya
 git pull
 else
-git clone https://github.com/Havoc-Devices/android_kernel_xiaomi_surya.git  $HOME/kernel/xiaomi/surya
+git clone https://github.com/ArrowOS-Devices/android_kernel_xiaomi_surya.git  $HOME/kernel/xiaomi/surya
 fi
 # vendor
 if [[ -e vendor/xiaomi/surya ]];then
 cd $HOME/vendor/xiaomi/surya
 git pull
 else
-git clone https://github.com/Havoc-Devices/android_vendor_xiaomi_surya $HOME/vendor/xiaomi/
+git clone https://github.com/Surya-Trees-SC/vendor_xiaomi_surya-sc $HOME/vendor/xiaomi/surya
 fi
 # hardware
 if [[ -e hardware/xiaomi ]];then
